89651ed58e515119eb3f34d9db657126a3319c7f
w-t-w <15866369958@163.com>
2023-10-07 13:35:06 +0800

    accumulation: think of nodejs

---
+++ download/index.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const fs = require('fs');
+const Koa = require('koa');
+const Koa_mount = require('koa-mount');
+const Koa_static = require('koa-static');
+
+const PORT = 7777;
+
+const STATIC_DIR = path.resolve(process.cwd(), './download/source');
+const TEMPLATE_DIR = path.resolve(process.cwd(), './download/template/index.html');
+
+const buffer = fs.readFileSync(TEMPLATE_DIR);
+
+const koa = new Koa();
+
+koa.use(Koa_static(STATIC_DIR));
+
+koa.use(async (ctx, next) => {
+    const {request, response} = ctx;
+    const {url} = request;
+
+    if (url === '/favicon.ico') {
+        response.status = 200;
+        response.body = '';
+        return true;
+    }
+
+    await next();
+});
+
+koa.use(Koa_mount('/', ctx => {
+    const {response} = ctx;
+    response.status = 200;
+    response.type = 'html';
+    response.body = buffer;
+}));
+
+koa.listen(PORT, () => {
+    console.log(`The server is running at http://localhost:${PORT}!`);
+});
+
